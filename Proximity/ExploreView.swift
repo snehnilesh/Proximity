6644c49525b098a53137e15ea1cd6e5ad3e427e6
snehnilesh <86720256+snehnilesh@users.noreply.github.com>
2022-06-01 00:12:34 +0100

    Update ExploreView.swift

--- Proximity/ExploreView.swift
+++ Proximity/ExploreView.swift
@@ -18,34 +18,6 @@ struct ExploreView: View {
     let ProximityCircles = [100, 200, 350, 550, 800, 1100, 1450]
     var body: some View {
         ZStack {
-        
-//            ProximityAccountCircle()
-//                .padding(.bottom, 520)
-//                .padding(.leading, 20)
-//
-//            ProximityAccountCircle()
-//                .padding(.bottom, 175)
-//                .padding(.trailing, 275)
-//
-//            ProximityAccountCircle()
-//                .padding(.bottom, 250)
-//                .padding(.leading, 220)
-//
-//            ProximityAccountCircle()
-//                .padding(.top, 360)
-//                .padding(.trailing, 50)
-
-            VStack {
-                Text("Proximity")
-                    .font(.system(size: 30 , design: .rounded))
-                    .bold()
-                    .foregroundColor(Color.ProximityTitle)
-                Text("Connect with people nearby")
-                    .font(.system(size: 20 , design: .rounded))
-                    .bold()
-                    .foregroundColor(Color.ProximitySubtitle)
-            }
-			SafeAreaView()
         }
         .background(
             ForEach(0..<7) { num in
@@ -55,21 +27,6 @@ struct ExploreView: View {
             
             }
         )
-    }
-}
-
-struct ProximityAccountCircle: View {
-    var body: some View {
-        VStack {
-            Circle()
-                .foregroundColor(Color.ProximitySubtitle)
-                .shadow(color: Color.ExploreCirclesColor, radius: 5, x: 0, y: 0)
-                .frame(width: 75, height: 75)
-            Text("Sneh Nilesh")
-                .foregroundColor(Color.ProximitySubtitle)
-                .font(.system(size: 12 , design: .rounded))
-                .bold()
-        }
     }
 }
 
